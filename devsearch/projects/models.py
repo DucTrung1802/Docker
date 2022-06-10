5bc504c1fd996ebefbe76af21a2eebdab13fedb5
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-10 18:18:49 +0700

    #2 deploy a server via docker - complete

---
+++ devsearch/projects/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+# "null" is set to True if: we are allowed to create a record or a row in the database of this value and we don't need to set a description. (for Database)
+
+# "blank" is set to True means that whenever we're submitting some kind of form or making a post request, we can submit this value with this field being blank. (for Django)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (("up", "Up Vote"), ("down", "Down Vote"))
+    # owner =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return self.name
---
+++ devsearch/projects/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+projectList = [
+    {
+        "userId": "101",
+        "id": "1",
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+    {
+        "userId": "202",
+        "id": "2",
+        "title": "qui est esse",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    },
+    {
+        "userId": "303",
+        "id": "3",
+        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+]
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "projects/projects.html", context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    return render(
+        request,
+        "projects/single_project.html",
+        {"projectObj": projectObj},
+    )
