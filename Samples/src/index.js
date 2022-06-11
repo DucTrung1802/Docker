f7e2100644c242c9026c23d6f6df78907a25a437
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-12 04:38:17 +0700

    #5 working on make a login page connects with database

--- Samples/src/index.js
+++ Samples/src/index.js
@@ -1,4 +1,28 @@
+const DB_HOST = "db"
+const DB_USER = "user"
+const DB_PASS = "pass"
+const DB_DATABASE = "database"
+
 const express = require("express");
+ 
+const knex = require("knex")({
+  client: "postgresql",
+  connection: {
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_DATABASE
+  },
+});
+
+try {
+
+  // Create a table
+  await knex.schema
+  .createTable()
+}
+
+
 const app = express();
 const port = 3000;
 
